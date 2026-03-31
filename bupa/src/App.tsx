e0b6f8d29643af32253e7dd2584df32b61090929
ank <ank@ank.net.au>
2026-03-31 23:39:32 +1100

    Chakra-UI integration

--- bupa/src/App.tsx
+++ bupa/src/App.tsx
@@ -5,15 +5,24 @@ import BookOwners from "./components/BookOwners.tsx";
 import { queryClient } from "./api/api";
 import { QueryClientProvider } from "@tanstack/react-query";
 import ErrorBoundary from "./ErrorBoundary.tsx";
+import { Provider } from "./components/ui/provider.tsx";
 
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Suspense>
         <ErrorBoundary>
-          <BookOwners />
+          <PageRoot />
         </ErrorBoundary>
       </Suspense>
     </QueryClientProvider>
+  );
+};
+
+export const PageRoot = () => {
+  return (
+    <Provider>
+      <BookOwners />
+    </Provider>
   );
 };
