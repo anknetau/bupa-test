a738eb3098963274d4155effcbbc11f3cc770ff9
ank <ank@ank.net.au>
2026-04-01 09:07:35 +1100

    Have a single sample response JSON

--- bupa/src/api/types.test.ts
+++ bupa/src/api/types.test.ts
@@ -1,82 +1,9 @@
 import { expect, test } from "vitest";
 import { BookOwnerListSchema } from "./types";
-
-const sample_json = `[
-  {
-    "name": "Jane",
-    "age": 23,
-    "books": [
-      {
-        "name": "Hamlet",
-        "type": "Hardcover"
-      },
-      {
-        "name": "Wuthering Heights",
-        "type": "Paperback"
-      }
-    ]
-  },
-  {
-    "name": "Charlotte",
-    "age": 14,
-    "books": [
-      {
-        "name": "Hamlet",
-        "type": "Paperback"
-      }
-    ]
-  },
-  {
-    "name": "Max",
-    "age": 25,
-    "books": [
-      {
-        "name": "React: The Ultimate Guide",
-        "type": "Hardcover"
-      },
-      {
-        "name": "Gulliver's Travels",
-        "type": "Hardcover"
-      },
-      {
-        "name": "Jane Eyre",
-        "type": "Paperback"
-      },
-      {
-        "name": "Great Expectations",
-        "type": "Hardcover"
-      }
-    ]
-  },
-  {
-    "name": "William",
-    "age": 15,
-    "books": [
-      {
-        "name": "Great Expectations",
-        "type": "Hardcover"
-      }
-    ]
-  },
-  {
-    "name": "Charles",
-    "age": 17,
-    "books": [
-      {
-        "name": "Little Red Riding Hood",
-        "type": "Hardcover"
-      },
-      {
-        "name": "The Hobbit",
-        "type": "Ebook"
-      }
-    ]
-  }
-]
-`;
+import sample_response from "./sample_response.json";
 
 test("BookOwnerList response is mapped to the expected types", () => {
-  const bookOwnerList = BookOwnerListSchema.parse(JSON.parse(sample_json));
+  const bookOwnerList = BookOwnerListSchema.parse(sample_response);
   expect(bookOwnerList.length).toBe(5);
   expect(bookOwnerList[4].age).toBe(17);
   expect(bookOwnerList[4].books[1].name).toBe("The Hobbit");
