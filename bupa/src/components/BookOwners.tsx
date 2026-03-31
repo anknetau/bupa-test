6e420e96b1d9a2eee85f0ba813aa63fbf3761eeb
ank <ank@ank.net.au>
2026-03-31 22:43:21 +1100

    Add basic setup

---
+++ bupa/src/components/BookOwners.tsx
@@ -0,0 +1,40 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import {} from "../api";
+import type { BookOwner } from "../api/types";
+
+const Spinner = () => {
+  return <>🌀</>;
+};
+
+const BookOwners = () => {
+  const { data, isFetching, error } = useSuspenseQuery<[BookOwner]>({
+    queryKey: ["/test"],
+  });
+
+  if (error) return <div>"An error has occurred: " + error.message</div>;
+
+  return (
+    <div>
+      <h1>Test {isFetching ? <Spinner /> : null}</h1>
+      {data.map((bookOwner) => (
+        <p key={bookOwner.name}>
+          {/* <Button
+            onClick={() => {
+              // Prefetch the project query
+              queryClient.prefetchQuery({
+                queryKey: ["project", project.full_name],
+                queryFn: () => fetchProject(project.full_name),
+              });
+              setActiveProject(project.full_name);
+            }}
+          >
+            Load
+          </Button>{" "} */}
+          {bookOwner.age}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default BookOwners;
--- bupa/src/main.tsx
+++ bupa/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import BookOwners from "./components/BookOwners.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { queryClient } from "./api/api";
+import { QueryClientProvider } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Suspense>
+        <ErrorBoundary>
+          <BookOwners />
+        </ErrorBoundary>
+      </Suspense>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
