9bfb9f9feae986b024f7392543ac4e862a34592d
ank <ank@ank.net.au>
2026-04-01 09:22:58 +1100

    Moving to semantic base w/chakra

---
+++ bupa/src/components/chakra_base.tsx
@@ -0,0 +1,3 @@
+import { chakra } from "@chakra-ui/react";
+
+export const Main = chakra("main");
--- bupa/src/pages/BookOwnersPage.tsx
+++ bupa/src/pages/BookOwnersPage.tsx
@@ -1,5 +1,23 @@
+import { Button, HStack } from "@chakra-ui/react";
 import BookOwners from "../components/BookOwners";
+import { Main } from "../components/chakra_base";
 
 export const BookOwnersPage = () => {
-  return <BookOwners />;
+  return (
+    <>
+      <BookOwners />
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+      <Main>
+        <section>
+          <article>
+            <h2>Title</h2>
+            <p>Content</p>
+          </article>
+        </section>
+      </Main>
+    </>
+  );
 };
