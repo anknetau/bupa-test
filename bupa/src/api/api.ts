e807b8704168008bab9c66da404ba81e7bff43d3
ank <ank@ank.net.au>
2026-03-31 19:18:11 +1100

    Export URLs properly

--- bupa/src/api/api.ts
+++ bupa/src/api/api.ts
@@ -1,2 +1,3 @@
-const LOCAL_URL = "https://localhost";
-const PROD_URL = "https://digitalcodingtest.bupa.com.au/api/v1/bookowners";
+export const LOCAL_URL = "https://localhost";
+export const PROD_URL =
+  "https://digitalcodingtest.bupa.com.au/api/v1/bookowners";
