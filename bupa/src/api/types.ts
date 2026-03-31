4b4cfc2b4af5d9088436f24367d2e74b44108968
ank <ank@ank.net.au>
2026-03-31 22:39:06 +1100

    Clean up definition of types

--- bupa/src/api/types.ts
+++ bupa/src/api/types.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 
 export const BookSchema = z.object({
+  // Assumption: that these are never absent
   name: z.string(),
+  // Assumption: that these are the only 3 types in this response
   type: z.enum(["Hardcover", "Paperback", "Ebook"]),
 });
 
 export const BookOwnerSchema = z.object({
+  // Assumption: that these are never absent
   name: z.string(),
   age: z.number(),
   books: z.array(BookSchema),
@@ -14,4 +17,4 @@ export const BookOwnerSchema = z.object({
 export const BookOwnerListSchema = z.array(BookOwnerSchema);
 
 export type Book = z.infer<typeof BookSchema>;
-export type Person = z.infer<typeof BookOwnerSchema>;
+export type BookOwner = z.infer<typeof BookOwnerSchema>;
