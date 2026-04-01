b23bec93a57719dd0913d505b9a6b6f41bc5af9b
ank <ank@ank.net.au>
2026-04-02 00:00:20 +1100

    Mobile breakpoint at 600px

--- bupa/src/components/ui/ChakraAppProvider.tsx
+++ bupa/src/components/ui/ChakraAppProvider.tsx
@@ -1,12 +1,20 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./ColorModeProvider";
 
+const system = createSystem(defaultConfig, {
+  theme: {
+    breakpoints: {
+      md: "600px",
+    },
+  },
+});
+
 export function ChakraAppProvider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       {/* Assumption: this will be in light mode as per design. */}
       <ColorModeProvider {...props} forcedTheme="light" />
     </ChakraProvider>
