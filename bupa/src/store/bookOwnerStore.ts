ac56f9311a08add036e1f7701ce84b2d0da62d42
ank <ank@ank.net.au>
2026-04-01 20:55:32 +1100

    More work on the books store

--- bupa/src/store/bookOwnerStore.ts
+++ bupa/src/store/bookOwnerStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import type { BookOwner } from "@/api/types";
+import type { Book, BookOwner } from "@/api/types";
 
 export const useBookOwnerStore = create((set) => ({
   bookOwnerList: [] as BookOwner[],
@@ -10,6 +10,55 @@ export const useBookOwnerStore = create((set) => ({
 // localeCompare.
 export const bookOwnerListSortedByName = (bookOwnerList: BookOwner[]) => {
   return [...bookOwnerList].sort((a: BookOwner, b: BookOwner) => {
+    return a.name.localeCompare(b.name);
+  });
+};
+
+// This will make a synthetic book ID from a book's title and .
+// Assumption: that there's no better ID to use.
+export const makeBookId = (book: Book): string => {
+  let type = "";
+  switch (book.type) {
+    case "Ebook":
+      type = "E";
+      break;
+    case "Hardcover":
+      type = "H";
+      break;
+    case "Paperback":
+      type = "P";
+      break;
+    default:
+      type = "" as never;
+      break;
+  }
+  return book.name + "-" + type;
+};
+
+export const uniqueStrings = (strings: string[]): string[] => {
+  return strings.reduce(
+    (acc, s) => (acc.includes(s) ? acc : [...acc, s]),
+    [] as string[],
+  );
+};
+
+export const uniqueBooks = (data: BookOwner[]): Book[] => {
+  const idsSeen: string[] = [];
+  const result: Book[] = [];
+  data.forEach((bookOwner) => {
+    bookOwner.books.forEach((book) => {
+      const bookId = makeBookId(book);
+      if (!idsSeen.includes(bookId)) {
+        idsSeen.push(bookId);
+        result.push(book);
+      }
+    });
+  });
+  return result;
+};
+
+export const bookListSortedByTitle = (bookList: Book[]) => {
+  return [...bookList].sort((a: Book, b: Book) => {
     return a.name.localeCompare(b.name);
   });
 };
