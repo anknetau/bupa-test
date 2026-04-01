19602134af6eb435ff7b6146f02f268742046094
Andres Kievsky <andres.kievsky@gmail.com>
2026-04-01 14:37:41 +1100

    Add footer+header for layout

---
+++ bupa/src/components/layout/Footer.tsx
@@ -0,0 +1,5 @@
+import { chakra } from "@chakra-ui/react";
+
+export const Footer = () => {
+  return <chakra.footer id="footer"></chakra.footer>;
+};
---
+++ bupa/src/components/layout/Header.tsx
@@ -0,0 +1,5 @@
+import { chakra } from "@chakra-ui/react";
+
+export const Header = () => {
+  return <chakra.header id="header"></chakra.header>;
+};
