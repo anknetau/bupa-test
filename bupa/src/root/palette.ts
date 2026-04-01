e670fad05aaf28559ab51d7378b0ac4e8de4fcbe
ank <ank@ank.net.au>
2026-04-01 20:56:12 +1100

    Add simple palette

---
+++ bupa/src/root/palette.ts
@@ -0,0 +1,13 @@
+// For expediency, I've defined these separately instead of taking the time to consolidate a proper Chakra theme --Andy
+export const palette = {
+  bupaBlue: "#0079c8",
+  white: "white",
+};
+
+export const semanticColour = {
+  headerBackground: palette.bupaBlue,
+  headerTextColor: palette.white,
+  spinnerColour: palette.bupaBlue,
+  buttonBackground: palette.bupaBlue,
+  link: palette.bupaBlue,
+};
