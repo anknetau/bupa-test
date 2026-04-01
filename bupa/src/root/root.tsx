151ddfc5a1e34d13e580702dcb75db07230f6f7c
Andres Kievsky <andres.kievsky@gmail.com>
2026-04-02 00:23:01 +1100

    Delete useless code

--- bupa/src/root/root.tsx
+++ bupa/src/root/root.tsx
@@ -5,19 +5,6 @@ import "./index.css";
 import RootApp from "./components/RootApp.tsx";
 import { RootHtml } from "./components/RootHtml.tsx";
 
-// export const links: Route.LinksFunction = () => [
-//   { rel: "preconnect", href: "https://fonts.googleapis.com" },
-//   {
-//     rel: "preconnect",
-//     href: "https://fonts.gstatic.com",
-//     crossOrigin: "anonymous",
-//   },
-//   {
-//     rel: "stylesheet",
-//     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-//   },
-// ];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return <RootHtml>{children}</RootHtml>;
 }
